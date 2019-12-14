dff2b9a2d07d0dc7ee5acca2aeb4d3dde36a9074
Saman Rohanizade <saman.rohanizade@gmail.com>
2019-12-14 22:19:03 +0330

    create the App module for Ex2

---
+++ Ex2-Prototypal OOP (Particles animation with OLOO)/app.js
@@ -0,0 +1,8 @@
+let App = (function() {
+  let canvas = document.querySelector('.particles');
+  let ctx = canvas.getContext('2d');
+  let numberOfParticles = 105;
+  let particlesRGB = [115, 70, 40];
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
+})();
