664fdd5725b1859a52f09a97f1788f819d8402c2
Saman Rohanizade <saman.rohanizade@gmail.com>
2019-12-14 22:29:39 +0330

    add the Particle object with a delegation link to canvasDrawer

--- Ex2-Prototypal OOP (Particles animation with OLOO)/app.js
+++ Ex2-Prototypal OOP (Particles animation with OLOO)/app.js
@@ -1,4 +1,4 @@
-let App = (function() {
+let App = (function () {
   let canvas = document.querySelector('.particles');
   let ctx = canvas.getContext('2d');
   let numberOfParticles = 105;
@@ -6,18 +6,46 @@ let App = (function() {
   canvas.width = window.innerWidth;
   canvas.height = window.innerHeight;
 
+  // master object. responsible for drawing to the canvas
   let canvasDrawer = {
-    drawCircle: function() {
+    drawCircle: function () {
       ctx.beginPath();
       ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2, false);
       ctx.fillStyle = this.color;
       ctx.fill();
     },
-    isInHorizontalBound: function() {
+    isInHorizontalBound: function () {
       return (this.x > canvas.width || this.x < 0);
     },
-    isInVerticalBound: function() {
+    isInVerticalBound: function () {
       return (this.y > canvas.height || this.y < 0);
+    }
+  };
+
+  // the particle object. with a delegation link to the canvasDrawer
+  let particle = {
+    __proto__: canvasDrawer,
+    init: function (x, y, directionX, directionY, size, color) {
+      this.x = x;
+      this.y = y;
+      this.directionX = directionX;
+      this.directionY = directionY;
+      this.size = size;
+      this.color = color;
+    },
+
+    move: function () {
+      if (this.isInHorizontalBound()) {
+        this.directionX = -this.directionX;
+      }
+      if (this.isInVerticalBound()) {
+        this.directionY = -this.directionY;
+      }
+
+      this.x += this.directionX;
+      this.y += this.directionY;
+
+      this.drawCircle();
     }
   };
 })();
