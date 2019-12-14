e4ba41018fdba4d5e00f64c449f85e9438ef221f
Saman Rohanizade <saman.rohanizade@gmail.com>
2019-12-14 22:25:02 +0330

    add the canvasDrawer master object in Ex2

--- Ex2-Prototypal OOP (Particles animation with OLOO)/app.js
+++ Ex2-Prototypal OOP (Particles animation with OLOO)/app.js
@@ -5,4 +5,19 @@ let App = (function() {
   let particlesRGB = [115, 70, 40];
   canvas.width = window.innerWidth;
   canvas.height = window.innerHeight;
+
+  let canvasDrawer = {
+    drawCircle: function() {
+      ctx.beginPath();
+      ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2, false);
+      ctx.fillStyle = this.color;
+      ctx.fill();
+    },
+    isInHorizontalBound: function() {
+      return (this.x > canvas.width || this.x < 0);
+    },
+    isInVerticalBound: function() {
+      return (this.y > canvas.height || this.y < 0);
+    }
+  };
 })();
